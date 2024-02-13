46239f5bd56a3b6929bfed5db0473cbee4e237c0
valtsu23 <valtteri.eklund@gmail.com>
2024-02-14 01:10:32 +0200

    Add files via upload

---
+++ mcp3002.py
@@ -0,0 +1,32 @@
+import spidev
+
+# Enable SPI0.1 for adc
+spi = spidev.SpiDev(0, 0)
+spi.max_speed_hz = 1200000
+
+# Adc_ch 0/1
+def read_adc(adc_ch):
+    # Construct SPI message
+    #  First bit (Start): Logic high (1)
+    #  Second bit (SGL/DIFF): 1 to select single mode
+    #  Third bit (ODD/SIGN): Select channel (0 or 1)
+    #  Fourth bit (MSFB): 0 for LSB first
+    #  Next 12 bits: 0 (don't care)
+    msg = 0b11
+    msg = ((msg << 1) + adc_ch) << 5
+    msg = [msg, 0b00000000]
+    reply = spi.xfer2(msg)
+
+    # Construct single integer out of the reply (2 bytes)
+    adc = 0
+    for n in reply:
+        adc = (adc << 8) + n
+
+    # Last bit (0) is not part of ADC value, shift to remove it
+    adc = adc >> 1
+
+    # Return 0-1023
+    return adc
+
+def close():
+    spi.close()
---
+++ shift_light_v2.py
@@ -0,0 +1,64 @@
+import board
+import neopixel
+import time
+
+# Neopixel setup (pin 4, number of leds)
+pixels = neopixel.NeoPixel(board.D21, 8)
+
+# Turn off all the leds
+def leds_off():
+    pixels.fill((0, 0, 0))
+
+# Blink frequency and timer
+BLINK = .1
+t1 = time.monotonic()
+
+# Needed variables
+shift_changed = 10
+
+def action(rpm, STEP, END, br):
+    global shift_changed
+    global t1
+    shift = (END - rpm) // STEP + 1
+    # Blinker
+    if rpm > END + STEP:
+        if t1 + BLINK < time.monotonic():
+            pixels.fill((0, br, br))
+        if t1 + BLINK * 2 < time.monotonic():
+            pixels.fill((0, 0, 0))
+            t1 = time.monotonic()
+    # If shift light step is exceeded
+    elif shift_changed != shift:
+        # LED steps control
+        print("Shift:", shift)
+        print("Shift_changed:", shift_changed)
+        if shift <= 3:
+            pixels[0] = (0, br, 0)
+            pixels[7] = (0, br, 0)
+        else:
+            pixels[0] = (0, 0, 0)
+            pixels[7] = (0, 0, 0)
+
+        if shift <= 2:
+            pixels[1] = (0, br, 0)
+            pixels[6] = (0, br, 0)
+        else:
+            pixels[1] = (0, 0, 0)
+            pixels[6] = (0, 0, 0)
+
+        if shift <= 1:
+            pixels[2] = (br, br, 0)
+            pixels[5] = (br, br, 0)
+        else:
+            pixels[2] = (0, 0, 0)
+            pixels[5] = (0, 0, 0)
+
+        if shift <= 0:
+            pixels[3] = (br, 0, 0)
+            pixels[4] = (br, 0, 0)
+        else:
+            pixels[3] = (0, 0, 0)
+            pixels[4] = (0, 0, 0)
+
+    # Save the new state
+    shift_changed = shift
